7cc79ac03be4553d24268925e7fb7c99ac1aceb9
Dmitriy Iskra <dmitriyiskra@mail.ru>
2024-06-07 20:25:15 +0300

    initial commit

---
+++ resources/views/groups.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <pre>
+        {{data}}
+    </pre>
+    <div>Создание новой группы</div>
+    <a href="/groups/create" class="href"></a>
+</body>
+</html>
